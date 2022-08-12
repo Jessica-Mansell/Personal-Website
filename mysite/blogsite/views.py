0f59a3e50e7cbc9f57b3778ca93831d4992ac83b
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-11 21:52:02 -0500

    add blog post page

--- mysite/blogsite/views.py
+++ mysite/blogsite/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 # ListView allows us to list a query set into the database or display multiple blog posts at once
-# DetailView only pulls a single item from the database
+# DetailView only pulls a single item from the database or in this case one blog post per page
 # See more here: https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/
 
 # Create your views here.
@@ -16,3 +16,8 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
