498ad48ce1fae2310254e506166dde2f5c5d5773
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-11 22:19:50 -0500

    style form with bootstrap

--- mysite/blogsite/views.py
+++ mysite/blogsite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
+
 # ListView allows us to list a query set into the database or display multiple blog posts at once
 # DetailView only pulls a single item from the database or in this case one blog post per page
 # See more here: https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/
@@ -19,5 +21,6 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+
