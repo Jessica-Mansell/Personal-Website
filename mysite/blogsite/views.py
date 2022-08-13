5edf876b9e81e864ac8eb1e7a66c876c519816e8
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-13 14:52:03 -0500

    update and edit blog post

--- mysite/blogsite/views.py
+++ mysite/blogsite/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 # ListView allows us to list a query set into the database or display multiple blog posts at once
 # DetailView only pulls a single item from the database or in this case one blog post per page
 # See more here: https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/
-
-# Create your views here.
-# def index(request):
-#     return render(request, 'index.html', {})
 
 class HomeView(ListView):
     model = Post
@@ -24,3 +20,9 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
 
+class UpdatePostView(UpdateView):
+    model = Post
+    # can only use either form_class or fields at once so, choose wisely!
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'title_tag', 'body']
