1900c796e25122026812d080feed55f7b1f8b568
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-13 15:07:28 -0500

    delete post functions

--- mysite/blogsite/views.py
+++ mysite/blogsite/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # ListView allows us to list a query set into the database or display multiple blog posts at once
 # DetailView only pulls a single item from the database or in this case one blog post per page
+# CreateView lets us create blog posts
+# UpdateView lets us edit blog posts
+# DeleteView deletes the post
 # See more here: https://docs.djangoproject.com/en/4.1/ref/class-based-views/generic-display/
 
 class HomeView(ListView):
     model = Post
     template_name = 'index.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -26,3 +31,8 @@ class UpdatePostView(UpdateView):
     form_class = EditForm
     template_name = 'update_post.html'
     # fields = ['title', 'title_tag', 'body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
