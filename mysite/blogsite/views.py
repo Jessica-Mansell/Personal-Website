72e032e3808b1a0fcb3053020524ef31963b241f
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-15 23:03:58 -0500

    added categories to one view

--- mysite/blogsite/views.py
+++ mysite/blogsite/views.py
@@ -15,6 +15,13 @@ class HomeView(ListView):
     model = Post
     template_name = 'index.html'
     ordering = ['-post_date']
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+        
 # cats is the name of the variable containing the string used in the urls.py file and this must match.
 def CategoryView(request, cats):
     category_posts = Post.objects.filter(category__iexact=cats.title().replace('-', ' '))
