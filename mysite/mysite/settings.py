6bfe52c38a68a54b963be3115a2f1df86238a134
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-24 17:23:40 -0500

    redundant

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,10 +124,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
