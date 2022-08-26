309294e41db0353a0bb28e2db4c02b77df4c2e47
Jessica-Mansell <jcmansell88@gmail.com>
2022-08-26 12:31:39 -0500

    add comment section

--- mysite/blogsite/models.py
+++ mysite/blogsite/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -50,3 +51,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
