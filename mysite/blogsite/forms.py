93345740fd0bce94e74eaaf6581ce5f0b425774c
Jessica-Mansell <jcmansell88@gmail.com>
2022-09-06 22:26:26 -0500

    forms sizing fixed

--- mysite/blogsite/forms.py
+++ mysite/blogsite/forms.py
@@ -15,13 +15,13 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'author', 'category' , 'body', 'snippet', 'header_image')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'blank_form'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'id': 'blank_form'}),
             'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'User', 'type':'hidden'}),
             # 'author': forms.Select(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-            'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choice_list, attrs={'class': 'form-control', 'id': 'blank_form'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'id': 'post_body_add'}),
+            'snippet': forms.Textarea(attrs={'class': 'form-control', 'id': 'snippet_add'}),
         }
 
 class EditForm(forms.ModelForm):
